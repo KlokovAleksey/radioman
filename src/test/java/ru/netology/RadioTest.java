09881bae523668dd22e57ebb473c81816c80ca91
Aleksey Klokov <lex-oz@yandex.ru>
2020-11-29 21:17:07 +0300

    goal check delete

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -9,39 +9,42 @@ class RadioTest {
     public void shouldCurrentStation() {
         Radio radio = new Radio();
         radio.setCurrentListeningStation(0);
-        assertEquals(0,radio.getCurrentListeningStation());
+        assertEquals(0, radio.getCurrentListeningStation());
     }
 
     @Test
-    public void shouldCurrentVolume(){
+    public void shouldCurrentVolume() {
         Radio radio = new Radio();
         radio.setCurrentVolume(0);
-        assertEquals(0,radio.getCurrentVolume());
-    }
-    @Test
-    public void shouldSetMinNumberStation(){
-        Radio radio = new Radio();
-radio.setMinNumberStation(0);
-assertEquals(0,radio.getMinNumberStation());
-    }
-    @Test
-    public void  shouldSetMaxNumberStation(){
-        Radio radio = new Radio();
-        radio.setMaxNumberStation(0);
-        assertEquals(0,radio.getMaxNumberStation());
-    }
-    @Test
-    public void shouldSetMinVolume(){
-        Radio radio = new Radio();
-        radio.setMinVolume(0);
-        assertEquals(0,radio.getMinVolume());
+        assertEquals(0, radio.getCurrentVolume());
     }
 
     @Test
-    public void shouldSetMaxVolume(){
+    public void shouldSetMinNumberStation() {
+        Radio radio = new Radio();
+        radio.setMinNumberStation(0);
+        assertEquals(0, radio.getMinNumberStation());
+    }
+
+    @Test
+    public void shouldSetMaxNumberStation() {
+        Radio radio = new Radio();
+        radio.setMaxNumberStation(0);
+        assertEquals(0, radio.getMaxNumberStation());
+    }
+
+    @Test
+    public void shouldSetMinVolume() {
+        Radio radio = new Radio();
+        radio.setMinVolume(0);
+        assertEquals(0, radio.getMinVolume());
+    }
+
+    @Test
+    public void shouldSetMaxVolume() {
         Radio radio = new Radio();
         radio.setMaxVolume(0);
-        assertEquals(0,radio.getMaxVolume());
+        assertEquals(0, radio.getMaxVolume());
     }
 
 }
