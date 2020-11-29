ec00f6223f85aeff92a57db9b2a26f650683fc58
Aleksey Klokov <lex-oz@yandex.ru>
2020-11-29 21:01:59 +0300

    test add

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -11,4 +11,37 @@ class RadioTest {
         radio.setCurrentListeningStation(0);
         assertEquals(0,radio.getCurrentListeningStation());
     }
+
+    @Test
+    public void shouldCurrentVolume(){
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        assertEquals(0,radio.getCurrentVolume());
+    }
+    @Test
+    public void shouldSetMinNumberStation(){
+        Radio radio = new Radio();
+radio.setMinNumberStation(0);
+assertEquals(0,radio.getMinNumberStation());
+    }
+    @Test
+    public void  shouldSetMaxNumberStation(){
+        Radio radio = new Radio();
+        radio.setMaxNumberStation(0);
+        assertEquals(0,radio.getMaxNumberStation());
+    }
+    @Test
+    public void shouldSetMinVolume(){
+        Radio radio = new Radio();
+        radio.setMinVolume(0);
+        assertEquals(0,radio.getMinVolume());
+    }
+
+    @Test
+    public void shouldSetMaxVolume(){
+        Radio radio = new Radio();
+        radio.setMaxVolume(0);
+        assertEquals(0,radio.getMaxVolume());
+    }
+
 }
